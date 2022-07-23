4c8046e2a0c90192a697792c5f7e97148aa36eb8
lorgiolazarte <53023179+lorgiolazarte@users.noreply.github.com>
2022-07-23 11:06:43 -0400

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -13,12 +13,24 @@ module.exports = async function main(callback) {
         // Set up a Truffle contract, representing our deployed Box instance
         const Bank = artifacts.require('Bank');
         const bank = await Bank.deployed();
+        //SET NAME
+        bank.setBankName('LORGI BANK')
         
+
         //const [owner] = await hre.ethers.getSigners(); 
+        console.log("Bank Name: ",await bank.bankName())
         console.log("BankContract deployed to:",bank.address);
         console.log("BankContract owner address:", (await bank.bankOwner()).toString());
         
- 
+        //deposit money account 0,1
+        await bank.depositMoney({from: accounts[0],value: 77});
+        await bank.depositMoney({from: accounts[1],value: 88});
+
+        console.log("Balance AC1: ",(await bank.getCustomerBalance({from: accounts[0]})).toString());
+        console.log("Balance AC2: ",(await bank.getCustomerBalance({from: accounts[1]})).toString());
+        
+        console.log("Total Bank Balance: ",(await bank.getBankBalance()).toString());
+
         callback(0);
     } catch (error) {
         console.log('ERROR DETAILS');
@@ -26,3 +38,4 @@ module.exports = async function main(callback) {
         callback(1);
     }
 };
+
